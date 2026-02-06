963e7052778f1d269fb42b66ca476097c8b4a01e
RIDA FATIMA <ridafatimaa.wahab7819@gmail.com>
2026-02-06 11:39:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+//================= CHAP 6 ==================
+
+     //-=======increment=========
+
+     //--------part1----------
+  //var num=1;
+  //var newNum=num++;
+  //alert(num++);
+   
+
+     //--------part2----------
+  //var num=8;
+  //var newNum=++num;
+  //alert(++num);
+
+      //=========decrement========
+
+     //--------part1----------
+  // var num=5;
+  //var newNum=num--;
+  //alert(num--);
+
+    //--------part2----------
+  // var num=8;
+  //var newNum=--num;
+  //alert(--num);
+
+//================= CHAP 7 ==================
+
+//========DMAS RULE=========
+//var totalcost=1+4*4;
+//alert(totalcost);
+
+//=========brackets solve======
+//var totalcost = (1 + 3) * 4; 
+//alert(totalcost);
+
+//================= CHAP 8==================
+
+//var message = "Thanks ";
+//var username="RIDA"
+ //var banger = "!";
+// alert(message + username + banger);
+
+
+//=============== CHAP 9 ==================
+    //var numberOfCats = prompt("How many cats?");
+   //var spec = numberOfCats;
+
+
+   //var ques = prompt("What is your name?");
+  // var spec= ques;
